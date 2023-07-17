fe34c57361f92f5d6a623f1c969f5b81c17e981f
confusinguser <25439218+confusinguser@users.noreply.github.com>
2023-07-17 09:56:47 +0200

    Move Units struct to units module

--- src/units.rs
+++ src/units.rs
@@ -74,3 +74,62 @@ impl UnitType {
         }
     }
 }
+
+#[derive(Debug, Default)]
+pub(crate) struct Units {
+    units: Vec<Unit>,
+}
+
+impl Units {
+    pub(crate) fn get_unit(&self, coords: CellCoordinates) -> Option<&Unit> {
+        self.units.iter().find(|unit| unit.coords == coords)
+    }
+
+    pub(crate) fn get_unit_mut(&mut self, coords: CellCoordinates) -> Option<&mut Unit> {
+        self.units.iter_mut().find(|unit| unit.coords == coords)
+    }
+
+    /// TODO: Make unit tests for this one
+    pub(crate) fn get_units_mut(
+        &mut self,
+        coords_list: Vec<CellCoordinates>,
+    ) -> Vec<Option<&mut Unit>> {
+        let mut found_units = self
+            .units
+            .iter_mut()
+            .filter(|unit| coords_list.iter().any(|coords| *coords == unit.coords))
+            .collect::<Vec<&mut Unit>>();
+
+        let mut output = Vec::with_capacity(coords_list.len());
+        output.resize_with(coords_list.len(), || None);
+        for coords in coords_list {
+            let position = found_units.iter().position(|unit| unit.coords == coords);
+            if let Some(position) = position {
+                output[position] = Some(found_units.remove(position));
+            }
+        }
+        output
+    }
+
+    pub(crate) fn get_unit_from_entity(&self, entity: Entity) -> Option<&Unit> {
+        self.units.iter().find(|unit| {
+            if let Some(unit_entity) = unit.entity {
+                unit_entity == entity
+            } else {
+                false
+            }
+        })
+    }
+
+    pub(crate) fn is_unit_at(&self, coords: CellCoordinates) -> bool {
+        self.units.iter().any(|unit| unit.coords == coords)
+    }
+
+    pub(crate) fn remove_dead_units(&mut self) {
+        self.units.retain(|unit| !unit.dead)
+    }
+
+    pub(crate) fn add_unit(&mut self, unit: Unit) {
+        self.units.push(unit)
+    }
+}
